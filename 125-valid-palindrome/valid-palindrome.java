a3b116f8489014a8592dbf1d78e9f52af42c8c7e
Nagesh Anil Yavalkar <nageshyavalkar@gmail.com>
2026-09-08 23:27:08 +0530

    Time: 3 ms (56.75%) | Memory: 44.5 MB (53.77%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+    int left = 0;
+    int right = s.length()-1;
+   while(left<right){
+    char l = s.charAt(left);
+    char r = s.charAt(right);
+    if(!isAlphaNum(l)){
+        left++;
+    }
+    else if(!isAlphaNum(r)){
+        right--;
+    }else{
+        if(Character.toLowerCase(l)!=Character.toLowerCase(r)){
+            return false;
+
+        }
+        left++;
+        right--;
+    }
+   }return true;
+   
+ 
+        
+    }  private boolean isAlphaNum(char r){
+    return (r>='a'&& r<='z')||(r>='A'&&r<='Z')||(r>='0'&&r<='9');
+   }
+}
