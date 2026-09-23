0d33e4046a603d27f9af1a1789e8b63ebe0dc69a
Nagesh Anil Yavalkar <nageshyavalkar@gmail.com>
2026-09-23 23:43:39 +0530

    Time: 4 ms (100.00%) | Memory: 46.1 MB (16.55%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        int n = x;
+        int rev = 0;
+        while(n>0){
+            int r = n%10;
+            rev = rev*10+r;
+            n/=10;
+
+        
+        }
+        if(rev==x){
+            return true;
+        }
+        return false;
+     
+        
+        
+        
+    }
+}
